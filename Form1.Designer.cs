0c6f18c6276cd898bd67dad5a2cbf3c10a97932b
Azra Şimşek <azrasmsk33@gmail.com>
2022-12-05 14:14:00 +0300

    Giriş butonu fonksiyonları eklendi.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,66 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sifreGiristxt = new System.Windows.Forms.TextBox();
+            this.adGiristxt = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.personelGirisbtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // sifreGiristxt
             // 
-            this.textBox1.Location = new System.Drawing.Point(322, 190);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(278, 22);
-            this.textBox1.TabIndex = 0;
+            this.sifreGiristxt.Location = new System.Drawing.Point(206, 199);
+            this.sifreGiristxt.Name = "sifreGiristxt";
+            this.sifreGiristxt.Size = new System.Drawing.Size(278, 22);
+            this.sifreGiristxt.TabIndex = 0;
             // 
-            // textBox2
+            // adGiristxt
             // 
-            this.textBox2.Location = new System.Drawing.Point(322, 133);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(278, 22);
-            this.textBox2.TabIndex = 1;
+            this.adGiristxt.Location = new System.Drawing.Point(206, 151);
+            this.adGiristxt.Name = "adGiristxt";
+            this.adGiristxt.Size = new System.Drawing.Size(278, 22);
+            this.adGiristxt.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(222, 139);
+            this.label1.Location = new System.Drawing.Point(94, 157);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(75, 16);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Yetkili Adı  :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(222, 196);
+            this.label2.Location = new System.Drawing.Point(94, 199);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(43, 16);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Şifre : ";
             // 
-            // button1
+            // personelGirisbtn
             // 
-            this.button1.Location = new System.Drawing.Point(382, 232);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(217, 41);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.personelGirisbtn.BackColor = System.Drawing.Color.LawnGreen;
+            this.personelGirisbtn.Location = new System.Drawing.Point(267, 243);
+            this.personelGirisbtn.Name = "personelGirisbtn";
+            this.personelGirisbtn.Size = new System.Drawing.Size(217, 41);
+            this.personelGirisbtn.TabIndex = 4;
+            this.personelGirisbtn.Text = "Giriş";
+            this.personelGirisbtn.UseVisualStyleBackColor = false;
+            this.personelGirisbtn.Click += new System.EventHandler(this.personelGirisbtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(206, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 394);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(556, 349);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.personelGirisbtn);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.adGiristxt);
+            this.Controls.Add(this.sifreGiristxt);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Ana Sayfa";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,11 +114,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox sifreGiristxt;
+        private System.Windows.Forms.TextBox adGiristxt;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button personelGirisbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace KutuphaneOtomasyonu1
         {
             InitializeComponent();
         }
+
+        private void personelGirisbtn_Click(object sender, EventArgs e)
+        {
+            string gelenAd = adGiristxt.Text;
+            string gelenSifre = sifreGiristxt.Text;
+
+            if(gelenAd.Equals("admin") && gelenSifre.Equals("2412"))
+            {
+                MessageBox.Show(text: "Başarılı bir şekilde giriş yaptınız.");
+            }
+            else
+            {
+                MessageBox.Show(text: "Kullanıcı adı veya şifre hatalı.");
+            }
+        }
     }
 }
